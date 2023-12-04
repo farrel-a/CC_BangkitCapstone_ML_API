5df8459e7d8fd885720742881d4a7e99fa322e08
farrel-a <13520110@std.stei.itb.ac.id>
2023-12-04 20:25:41 +0700

    feat: add base64 decoder

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# CC_BangkitCapstone_ML_API
+# Bangkit Capstone ML API
 REST API for ML Model
+
+## How to run
+1. Install dependencies
+```bash
+pip install requirements.txt
+```
+
+2. Run the app use python version >=3.8
+```bash
+python app.py
+```
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, json
-import random
+import random, base64
 
 app = Flask(__name__)
 
@@ -10,7 +10,18 @@ def get_home_handler():
 @app.route("/predict", methods=["POST"])
 def preidct_handler():
     req = json.loads(request.data)
-    return jsonify({"success": True, "msg": f"prediction is {random.randint(1,100)}", "data" : req["data"]}), 200
+    data = req["data"]
+    b64_img_data = data["b64_img_data"]
+    decoded_data = base64.b64decode(b64_img_data)
+    filename = data["filename"]
+
+    # for debugging, uncomment to save the file from decoded b64
+    # with open(filename, "wb") as f:
+    #     f.write(decoded_data)
+
+    # TODO: Image processing here
+    
+    return jsonify({"success": True, "msg": f"prediction is {random.randint(1,100)}"}), 200
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
